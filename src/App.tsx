dd1ed81999dea04d6d9f07963549a60e996a3338
clsc411 <clarascalzer@gmail.com>
2024-09-17 14:40:44 -0400

    9/17 updated styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,90 @@
 import React from "react";
 import "./App.css";
+import { Button } from "react-bootstrap";
 
 function App(): React.JSX.Element {
     return (
         <div className="App">
-            <header className="App-header">
-                UM COS420 with React Hooks and TypeScript
-            </header>
+            <header className="App-header">Welcome to my website!</header>
             <p>
                 Edit <code>src/App.tsx</code> and save. This page will
-                automatically reload. Clara
+                automatically reload. Clara Hello World
             </p>
+
+            {/* Organizing content into flex "columns" */}
+            <div
+                style={{
+                    display: "flex",
+                    justifyContent: "space-between",
+                    alignItems: "center",
+                    marginTop: "20px",
+                }}
+            >
+                <div>
+                    <img
+                        src={"./IMG_5673.JPG"}
+                        alt="This is a lovely picture of my cat Milton"
+                        style={{ maxWidth: "200px" }}
+                    />
+                </div>
+                <div
+                    style={{
+                        width: "100px",
+                        height: "100px",
+                        backgroundColor: "red",
+                    }}
+                ></div>
+            </div>
+
+            <div
+                style={{
+                    display: "flex",
+                    justifyContent: "space-between",
+                    alignItems: "center",
+                    marginTop: "20px",
+                }}
+            >
+                <ol>
+                    <li>My</li>
+                    <li>First</li>
+                    <li>Website</li>
+                </ol>
+                <div
+                    style={{
+                        width: "100px",
+                        height: "100px",
+                        backgroundColor: "red",
+                    }}
+                ></div>
+            </div>
+
+            <div
+                style={{
+                    display: "flex",
+                    justifyContent: "space-between",
+                    alignItems: "center",
+                    marginTop: "20px",
+                }}
+            >
+                <p>
+                    Hello! My name is Clara Scalzer. I am a student in COS420{" "}
+                </p>
+                <div
+                    style={{
+                        width: "100px",
+                        height: "100px",
+                        backgroundColor: "red",
+                    }}
+                ></div>
+            </div>
+
+            <Button
+                onClick={() => {
+                    console.log("Hello World!");
+                }}
+            >
+                Log Hello World
+            </Button>
         </div>
     );
 }
